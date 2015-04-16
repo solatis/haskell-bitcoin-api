f567ead6f4652dcd4b464074f321ff7e119f153d
Leon Mergen <leon@solatis.com>
2015-04-16 14:23:28 +0700

    Implements test case for authentication errors

--- test/Network/Bitcoin/ClientSpec.hs
+++ test/Network/Bitcoin/ClientSpec.hs
@@ -1,20 +1,32 @@
+{-# LANGUAGE LambdaCase #-}
+
 module Network.Bitcoin.ClientSpec where
 
-import qualified Data.Text as T ( pack )
+import qualified Data.Text                as T (pack)
 
+import           Network.HTTP.Client      (HttpException (..))
+
+import           Network.Wreq.Lens (statusCode)
+import           Control.Lens ((^.))
 import           Network.Bitcoin.Client
-import           Test.Hspec
-
 import qualified Network.Bitcoin.Rpc.Misc as Misc
+import           Test.Hspec
 
 testClient :: (Client -> IO a) -> IO a
 testClient = withClient "127.0.0.1" 18332 (T.pack "user") (T.pack "pass")
+
+isStatusCodeException :: Int -> HttpException -> Bool
+isStatusCodeException code (StatusCodeException s _ _) = s ^. statusCode == code
+isStatusCodeException _ _ = False
 
 spec :: Spec
 spec = do
   describe "when creating a new client session" $ do
     it "callback returns generated value" $ do
       testClient (\_ -> return "foo") `shouldReturn` "foo"
+
+    it "fails when providing invalid authentication credentials" $ do
+      withClient "127.0.0.1" 18332 (T.pack "invaliduser") (T.pack "invalidpass") Misc.getInfo `shouldThrow` isStatusCodeException 401
 
   describe "when testing miscelaneous functions" $ do
    it "should be able to return server info" $ do
