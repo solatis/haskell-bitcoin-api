9d567e80cdf26ce13d3912139ce4835008c10683
Leon Mergen <leon@solatis.com>
2015-05-03 16:22:45 +0700

    Adds additional documentation

--- src/Network/Bitcoin/Api/Blockchain.hs
+++ src/Network/Bitcoin/Api/Blockchain.hs
@@ -12,17 +12,25 @@ import qualified Data.Bitcoin.Types           as BT
 import qualified Network.Bitcoin.Api.Internal as I
 import qualified Network.Bitcoin.Api.Types    as T
 
+-- | Gets the amount of blocks currently in the blockchain, also known as the
+--   'height' of the blockchain.
 getBlockCount :: T.Client -> IO Integer
 getBlockCount client =
   I.call client "getblockcount" emptyArray
 
-getBlockHash :: T.Client -> Integer -> IO BT.BlockHash
+-- | Get the hash of a block based on its offset (height).
+getBlockHash :: T.Client        -- ^ Our client context
+             -> Integer         -- ^ The height/offset of the block. 0 is the genesis block.
+             -> IO BT.BlockHash -- ^ The hash of the block
 getBlockHash client offset =
   let configuration = [toJSON offset]
 
   in I.call client "getblockhash"  configuration
 
-getBlock :: T.Client -> HS.HexString -> IO Btc.Block
+-- | Gets a block based on its hash.
+getBlock :: T.Client     -- ^ Our session context
+         -> HS.HexString -- ^ Hexadecimal representation of the hash of a block
+         -> IO Btc.Block -- ^ The block
 getBlock client hash =
   let configuration = [toJSON hash, toJSON False]
 
