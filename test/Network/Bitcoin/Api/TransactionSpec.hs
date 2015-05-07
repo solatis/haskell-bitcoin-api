500183979f4fcec5957e7f385c7575675d050ec2
Leon Mergen <leon@solatis.com>
2015-05-07 16:36:09 +0700

    Adds test case for Transaction.watch

--- test/Network/Bitcoin/Api/TransactionSpec.hs
+++ test/Network/Bitcoin/Api/TransactionSpec.hs
@@ -1,8 +1,13 @@
 module Network.Bitcoin.Api.TransactionSpec where
 
-import           Data.Maybe                                   (mapMaybe)
-
 import           Control.Lens                                 ((^.))
+import Control.Concurrent (forkIO)
+import Control.Concurrent.MVar (newEmptyMVar,
+                                putMVar,
+                                takeMVar)
+import           Data.Conduit
+import qualified Data.Conduit.List                            as CL
+import           Data.Maybe                                   (mapMaybe)
 
 import qualified Data.Bitcoin.Transaction                     as Btc
 
@@ -90,4 +95,34 @@ spec = do
       length (txs) `shouldSatisfy` (>= 1)
 
     it "can watch for new transactions" $ do
-      True `shouldBe` True
+      testClient $ \client -> do
+
+        result <- newEmptyMVar
+
+        -- This forks a thread, consumes a single transaction from the conduit,
+        -- and puts it in the MVar.
+        _ <- forkIO $ do
+          list <- runConduit $ Transaction.watch client (Just 1) $= CL.take 1
+          putMVar result list
+
+        -- Now, let's generate a transaction and some blocks to put the
+        -- the transaction in.
+        utxs             <- Wallet.listUnspent client
+
+        (length utxs) `shouldSatisfy` (>= 1)
+
+        -- Calculate the total BTC of all unspent transactions
+        let btc          = foldr (+) 0 $ map (^. amount) utxs
+
+        addr             <- Wallet.newAddress client
+        tx               <- Transaction.create client utxs [(addr, (btc - 0.0001))]
+        (tx', completed) <- Transaction.sign client tx (Just utxs) Nothing
+
+        completed `shouldBe` True
+
+        _                <- Transaction.send client tx'
+        _                <- Mining.generate client 10
+
+        val <- takeMVar result
+
+        length (val) `shouldBe` 1
