32631281d8fb98b89c6c8d2a766658a26591ca23
Rune K. Svendsen <runesvend@gmail.com>
2016-11-25 23:24:28 +0100

    add txOutProof

--- src/Network/Bitcoin/Api/UTXO.hs
+++ src/Network/Bitcoin/Api/UTXO.hs
@@ -1,6 +1,7 @@
 module Network.Bitcoin.Api.UTXO where
 
 import           Data.Word           (Word32, Word64)
+import           Data.Maybe          (maybeToList)
 import           Data.Aeson
 
 import qualified Data.Bitcoin.Types                     as BT
@@ -8,11 +9,12 @@ import qualified Network.Bitcoin.Api.Internal           as I
 import qualified Network.Bitcoin.Api.Types              as T
 import qualified Network.Bitcoin.Api.Types.UnspentTxOut as UTXO
 import qualified Data.Base58String                      as B58S
+import qualified Data.HexString                         as HS
 
 type IncludeMempool = Bool
 
 -- | Returns details about an unspent transaction output.
-getTxOut :: T.Client     -- ^ Our session context
+getTxOut :: T.Client                        -- ^ Our session context
          -> BT.TransactionId                -- ^ Transaction ID of transaction containing output
          -> Word32                          -- ^ Vout/index of output in transaction
          -> IncludeMempool                  -- ^ Return transaction outputs from unconfirmed transactions as well?
@@ -21,3 +23,13 @@ getTxOut client txid index includeMempool =
   let configuration = [toJSON txid, toJSON index, toJSON includeMempool]
 
   in I.call client "gettxout" configuration
+
+-- | Returns proof that one or more transactions were included in a block
+txOutProof :: T.Client                  -- ^ Our session context
+           -> [BT.TransactionId]        -- ^ Transaction IDs to return proof for
+           -> Maybe BT.BlockHash        -- ^ Optionally look in only this block
+           -> IO (Maybe HS.HexString)   -- ^ Hex-encoded proof (see: https://bitcoin.org/en/developer-reference#merkleblock)
+txOutProof client txidLst hashM =
+  let configuration = toJSON txidLst : fmap toJSON (maybeToList hashM)
+
+  in I.call client "gettxoutproof" configuration
