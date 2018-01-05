cd4b9757283bdd61b5b4ac922db36bbebc469dea
Rune K. Svendsen <runesvend@gmail.com>
2018-01-05 20:10:17 +0100

    Test: isStatusCodeException: http-client >= 0.5 compat.

--- test/Network/Bitcoin/Api/TestUtil.hs
+++ test/Network/Bitcoin/Api/TestUtil.hs
@@ -2,7 +2,9 @@ module Network.Bitcoin.Api.TestUtil (testClient, isStatusCodeException) where
 
 import qualified Data.Text                                    as T (pack)
 import           Control.Lens                                 ((^.))
-import           Network.HTTP.Client                          (HttpException (..))
+import           Network.HTTP.Client                          (HttpException (..)
+                                                              ,HttpExceptionContent (..)
+                                                              ,responseStatus)
 import           Network.Wreq.Lens                            (statusCode)
 
 import           Network.Bitcoin.Api.Client
@@ -11,5 +13,6 @@ testClient :: (Client -> IO a) -> IO a
 testClient = withClient "127.0.0.1" 18332 (T.pack "user") (T.pack "pass")
 
 isStatusCodeException :: Int -> HttpException -> Bool
-isStatusCodeException code (StatusCodeException s _ _) = s ^. statusCode == code
+isStatusCodeException code (HttpExceptionRequest _ (StatusCodeException res _)) =
+  responseStatus res ^. statusCode == code
 isStatusCodeException _ _ = False
